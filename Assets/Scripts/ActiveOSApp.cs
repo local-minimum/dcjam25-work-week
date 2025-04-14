43145d8f50a7c4dd7aee3c06f3912ca0bba1eb90
Martin Zackrisson <martin.zackrisson@fripost.org>
2025-04-14 19:01:10 +0200

    Start of OS on computer

---
+++ Assets/Scripts/ActiveOSApp.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class ActiveOSApp : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI Title;
+
+    public void CloseApp()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void OpenApp()
+    {
+        gameObject.SetActive(true);
+        transform.SetAsLastSibling();
+    }
+}
