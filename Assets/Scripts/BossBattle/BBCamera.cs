8bf182e6a7ed11f071e9b406ba7746bfb29c7413
Martin Zackrisson <martin.zackrisson@fripost.org>
2025-04-19 17:45:05 +0200

    fix toilet stall opening direction

--- Assets/Scripts/BossBattle/BBCamera.cs
+++ Assets/Scripts/BossBattle/BBCamera.cs
@@ -5,8 +5,19 @@ public class BBCamera : MonoBehaviour
     [SerializeField]
     float speed = 1.5f;
 
+    [SerializeField]
+    float initialDelay = 3f;
+
+    float realtimeStart;
+    private void Start()
+    {
+        realtimeStart = Time.realtimeSinceStartup + initialDelay;
+    }
+
     private void Update()
     {
+        if (Time.realtimeSinceStartup < realtimeStart) return;
+
         transform.position += Vector3.right * speed * Time.deltaTime;
     }
 }
